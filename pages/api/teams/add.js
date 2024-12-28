360abfc5fb271fa17065c7c729901ec9e28b4516
arevythis <revythis.andreas@gmail.com>
2024-12-28 21:22:46 +0200

    Update add.js

--- pages/api/teams/add.js
+++ pages/api/teams/add.js
@@ -8,6 +8,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // Required for connecting to Supabase
+      sslmode: 'require'
   },
 });
 export default async function handler(req, res) {
