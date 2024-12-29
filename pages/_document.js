723719c9ce40f4f57479d3e1552c9f2f37dbe948
arevythis <revythis.andreas@gmail.com>
2024-12-29 21:29:30 +0200

    telos olwn

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,3 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
@@ -14,6 +13,15 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css"
           rel="stylesheet"
         />
+        
+        {/* Favicon link */}
+        <link rel="icon" href="/oly.png" />
+        {/* Optional: Add different favicon sizes for different devices */}
+        <link rel="icon" href="/oly.png" sizes="32x32" />
+        <link rel="icon" href="/oly.png" sizes="16x16" />
+        {/* Apple touch icon */}
+        <link rel="apple-touch-icon" href="/oly.png" sizes="180x180" />
+        <meta name="theme-color" content="#000000" />
       </Head>
       <body>
         <Main />
